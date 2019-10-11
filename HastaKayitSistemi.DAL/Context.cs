2754ec7a32c42d38bca571e268d615bbec83c067
ebru1111 <aydmr.ebru@hotmail.com>
2019-10-12 01:19:37 +0300

    mapping işlemleine eklemeler2

--- HastaKayitSistemi.DAL/Context.cs
+++ HastaKayitSistemi.DAL/Context.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,15 +19,22 @@ namespace HastaKayitSistemi.DAL
         public DbSet<Doktor> Doktorlar { get; set; }
         public DbSet<Eczaci> Eczacilar { get; set; }
         public DbSet<Hastane> Hastaneler { get; set; }
+        public DbSet<HastaDoktor> HastaDoktor { get; set; }
         public DbSet<Poliklinik> Poliklinik { get; set; }
         public DbSet<Randevu> Randevular { get; set; }
         public DbSet<Recete> Receteler { get; set; }
         public DbSet<Departman> Departmanlar { get; set; }
+
+
+
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-
             modelBuilder.Configurations.Add(new ReceteMapping());
+            modelBuilder.Configurations.Add(new HastaDoktorMapping());
             modelBuilder.Configurations.Add(new PoliklinikMapping());
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
             base.OnModelCreating(modelBuilder);
         }
     }
