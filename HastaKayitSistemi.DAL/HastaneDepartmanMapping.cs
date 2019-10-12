93aa0544d419661ed89cd4985aca3d730dbd8efc
fthkr <fatih4496@gmail.com>
2019-10-12 14:11:15 +0300

    .

--- HastaKayitSistemi.DAL/HastaneDepartmanMapping.cs
+++ HastaKayitSistemi.DAL/HastaneDepartmanMapping.cs
@@ -8,8 +8,6 @@ using System.Threading.Tasks;
 
 namespace HastaKayitSistemi.DAL
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
     public class HastaneDepartmanMapping : EntityTypeConfiguration<HastaneDepartman>
     {
 
@@ -18,20 +16,6 @@ namespace HastaKayitSistemi.DAL
             HasRequired(x => x.DepartmanHastaneleri).WithMany(x => x.HastaneDepartmanlari).HasForeignKey(x => x.DepartmanID); ;
             HasRequired(x => x.HastaneDepartmanlari).WithMany(x => x.DepartmanlarinHastaneleri).HasForeignKey(x => x.HastaneID); ;
         }
-=======
-=======
->>>>>>> c82eddbf63715436bac5d5c3723b59059af2ba2f
-    
-    public class HastaneDepartmanMapping : EntityTypeConfiguration<HastaneDepartman>
-    {
         
-        public HastaneDepartmanMapping()
-        {
-            
-        }    
-<<<<<<< HEAD
->>>>>>> c82eddbf63715436bac5d5c3723b59059af2ba2f
-=======
->>>>>>> c82eddbf63715436bac5d5c3723b59059af2ba2f
     }
 }
