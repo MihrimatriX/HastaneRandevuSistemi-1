36d70af93344cc65d101e20fdd6da163186afa85
fthkr <fatih4496@gmail.com>
2019-10-12 15:01:12 +0300

    hastamapping ve randevumapping eklendi
    bu ikisinin içinde datetime olduğu için ekledim

---
+++ HastaKayitSistemi.DAL/HastaMapping.cs
@@ -0,0 +1,19 @@
+﻿using HastaKayitSistemi.DATA;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HastaKayitSistemi.DAL
+{
+    public class HastaMapping : EntityTypeConfiguration<Hasta>
+    {
+        public HastaMapping()
+        {
+
+            Property(x => x.DogumTarihi).HasColumnType("datetime2");
+        }
+    }
+}
