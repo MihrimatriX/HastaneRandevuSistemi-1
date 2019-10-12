2d0bd44ce34c0cb2ca2a5e8689283d69084235b2
fthkr <fatih4496@gmail.com>
2019-10-13 00:54:12 +0300

    ÜYE OLMA EKRANI DEĞİŞTİRİLDİ.
    HASTA class ında e mail property si var. aynı zamanda baseclass tan kullanıcı adını alıcak. zaten kullanıcı adı email adresi. fazladan prop. var. bunu yarın hatırlatın düzeltelim. gereksiz içi boş bir class vardı onu sildim.

--- HastaKayitSistemi.UI/UyeOlmaEkranı.Designer.cs
+++ HastaKayitSistemi.UI/UyeOlmaEkranı.Designer.cs
@@ -29,35 +29,47 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.txtUyeAdi = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.txtUyeSifre = new System.Windows.Forms.TextBox();
+            this.txtSifre = new System.Windows.Forms.TextBox();
             this.btnOnayla = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtTcNo = new System.Windows.Forms.TextBox();
+            this.mskTxtTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.dtDogumTarihi = new System.Windows.Forms.DateTimePicker();
+            this.txtAdres = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label1.Location = new System.Drawing.Point(32, 36);
+            this.label1.Location = new System.Drawing.Point(58, 266);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.Size = new System.Drawing.Size(57, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Kullanıcı Adı :";
+            this.label1.Text = "E-mail:";
             // 
-            // txtUyeAdi
+            // txtEmail
             // 
-            this.txtUyeAdi.Location = new System.Drawing.Point(139, 38);
-            this.txtUyeAdi.Name = "txtUyeAdi";
-            this.txtUyeAdi.Size = new System.Drawing.Size(157, 20);
-            this.txtUyeAdi.TabIndex = 1;
+            this.txtEmail.Location = new System.Drawing.Point(136, 268);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(200, 20);
+            this.txtEmail.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(139, 13);
+            this.label2.Location = new System.Drawing.Point(133, 250);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(147, 13);
             this.label2.TabIndex = 2;
@@ -67,25 +79,25 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label3.Location = new System.Drawing.Point(83, 93);
+            this.label3.Location = new System.Drawing.Point(65, 321);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 20);
+            this.label3.Size = new System.Drawing.Size(46, 20);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Şifre :";
+            this.label3.Text = "Şifre:";
             // 
-            // txtUyeSifre
+            // txtSifre
             // 
-            this.txtUyeSifre.Location = new System.Drawing.Point(139, 95);
-            this.txtUyeSifre.Name = "txtUyeSifre";
-            this.txtUyeSifre.Size = new System.Drawing.Size(157, 20);
-            this.txtUyeSifre.TabIndex = 4;
+            this.txtSifre.Location = new System.Drawing.Point(136, 323);
+            this.txtSifre.Name = "txtSifre";
+            this.txtSifre.Size = new System.Drawing.Size(200, 20);
+            this.txtSifre.TabIndex = 4;
             // 
             // btnOnayla
             // 
             this.btnOnayla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnOnayla.Location = new System.Drawing.Point(139, 135);
+            this.btnOnayla.Location = new System.Drawing.Point(133, 366);
             this.btnOnayla.Name = "btnOnayla";
-            this.btnOnayla.Size = new System.Drawing.Size(157, 29);
+            this.btnOnayla.Size = new System.Drawing.Size(203, 29);
             this.btnOnayla.TabIndex = 5;
             this.btnOnayla.Text = "ONAYLA";
             this.btnOnayla.UseVisualStyleBackColor = true;
@@ -93,23 +105,138 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(142, 76);
+            this.label4.Location = new System.Drawing.Point(133, 307);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(234, 13);
             this.label4.TabIndex = 6;
             this.label4.Text = "Şİfre en az 8 karakter en çok 16 karakter olmalı.";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(88, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Adı:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(67, 99);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "TC No:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(63, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 20);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Soyadı:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(17, 132);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(107, 20);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Doğum Tarihi:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(58, 167);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(66, 20);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Telefon:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(69, 202);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(55, 20);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Adres:";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(136, 33);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(200, 20);
+            this.txtAd.TabIndex = 7;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(136, 65);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(200, 20);
+            this.txtSoyad.TabIndex = 7;
+            // 
+            // txtTcNo
+            // 
+            this.txtTcNo.Location = new System.Drawing.Point(136, 101);
+            this.txtTcNo.Name = "txtTcNo";
+            this.txtTcNo.Size = new System.Drawing.Size(200, 20);
+            this.txtTcNo.TabIndex = 7;
+            // 
+            // mskTxtTelefon
+            // 
+            this.mskTxtTelefon.Location = new System.Drawing.Point(136, 169);
+            this.mskTxtTelefon.Mask = "(999) 000-0000";
+            this.mskTxtTelefon.Name = "mskTxtTelefon";
+            this.mskTxtTelefon.Size = new System.Drawing.Size(200, 20);
+            this.mskTxtTelefon.TabIndex = 8;
+            // 
+            // dtDogumTarihi
+            // 
+            this.dtDogumTarihi.Location = new System.Drawing.Point(136, 131);
+            this.dtDogumTarihi.Name = "dtDogumTarihi";
+            this.dtDogumTarihi.Size = new System.Drawing.Size(200, 20);
+            this.dtDogumTarihi.TabIndex = 9;
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(136, 204);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(200, 20);
+            this.txtAdres.TabIndex = 7;
+            // 
             // UyeOlmaEkranı
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(393, 183);
+            this.ClientSize = new System.Drawing.Size(409, 478);
+            this.Controls.Add(this.dtDogumTarihi);
+            this.Controls.Add(this.mskTxtTelefon);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.txtTcNo);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAd);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.btnOnayla);
-            this.Controls.Add(this.txtUyeSifre);
+            this.Controls.Add(this.txtSifre);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtUyeAdi);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label1);
             this.Name = "UyeOlmaEkranı";
             this.Text = "UyeOlmaEkranı";
@@ -121,11 +248,23 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtUyeAdi;
+        private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtUyeSifre;
+        private System.Windows.Forms.TextBox txtSifre;
         private System.Windows.Forms.Button btnOnayla;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtTcNo;
+        private System.Windows.Forms.MaskedTextBox mskTxtTelefon;
+        private System.Windows.Forms.DateTimePicker dtDogumTarihi;
+        private System.Windows.Forms.TextBox txtAdres;
     }
 }
