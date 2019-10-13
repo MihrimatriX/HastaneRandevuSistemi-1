571a59da2b17c96147dc3e637a401f19d6048f2e
Furkan Pen <->
2019-10-13 14:13:41 +0300

    Update Sorgu.sql

--- Sorgu.sql
+++ Sorgu.sql
@@ -1,12 +1,4 @@
-﻿insert into Doktor values('Ahmet Yılmaz',1,1,'doktor','123',1,1)
-insert into Doktor values('Yeşim Avcı',2,1,'drYesim','123',2,1)
-insert into Doktor values('	Ramazan ﻿Acar',3,1,'drRamazan','123',3,2)
-insert into Doktor values('Irem Çakır',4,1,'drIrem','123',4,3)
-insert into Doktor values('Recep Can',5,1,'drRecep','123',5,4)
-insert into Doktor values('Ramazan Keskin',6,1,'drRamazan','123',6,4)
-insert into Doktor values('Salih Koç',7,1,'drSalih','123',7,4)
-insert into Doktor values('Cemal Şimşek',8,1,'drCemal','123',8,5)
-
+﻿
 insert into Departman values('ÇOCUK SAĞLIĞI VE HASTALIKLARI')
 insert into Departman values('GÖZ HASTALIKLARI')
 insert into Departman values('NÖROLOJİ')
@@ -15,13 +7,22 @@ insert into Departman values('KULAK-BURUN-BOĞAZ HASTALIKLARI')
 insert into Departman values('KARDİYOLOJİ')
 insert into Departman values('DİŞ HASTALIKLARI')
 
-insert into Hasta values('Adem','Çelik',12312344242,'İstanbul Bahçelievler','1996','222 22 222','ademcelik@mail.com',1,'sa',123)
-
 insert into Hastane values('Bakırköy Dr. Sadi Konuk Eğitim ve Araştırma Hastanesi')
 insert into Hastane values('Erenköy Ruh ve Sinir Hastalıkları Eğitim ve Araştırma Hastanesi')
 insert into Hastane values('Okmeydanı Eğitim ve Araştırma Hastanesi')
 insert into Hastane values('Bahçelievler Devlet Hastanesi')
 insert into Hastane values('Sancaktepe Ağız ve Diş Sağlığı Merkezi')
+
+insert into Doktor values('Ahmet Yılmaz',1,1,'doktor','123',1,1)
+insert into Doktor values('Yeşim Avcı',2,1,'drYesim','123',2,1)
+insert into Doktor values('	Ramazan ﻿Acar',3,1,'drRamazan','123',3,2)
+insert into Doktor values('Irem Çakır',4,1,'drIrem','123',4,3)
+insert into Doktor values('Recep Can',5,1,'drRecep','123',5,4)
+insert into Doktor values('Ramazan Keskin',6,1,'drRamazan','123',6,4)
+insert into Doktor values('Salih Koç',7,1,'drSalih','123',7,4)
+insert into Doktor values('Cemal Şimşek',7,1,'drCemal','123',7,5)
+
+insert into Hasta values('Adem','Çelik',12312344242,'İstanbul Bahçelievler','1996','222 22 222','ademcelik@mail.com',1,'sa',123)
 
 
 
